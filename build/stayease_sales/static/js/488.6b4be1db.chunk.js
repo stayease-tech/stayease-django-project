7dcf27c0fd7dacfdf4a54c47c3c5a28d67328807
stayease-tech <jibin@mystayease.com>
2026-04-08 11:31:05 +0530

    Delete build directory

--- build/stayease_sales/static/js/488.6b4be1db.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(globalThis.webpackChunkstayease_sales=globalThis.webpackChunkstayease_sales||[]).push([[488],{488(s,a,e){new Map,new Date}}]);
-//# sourceMappingURL=488.6b4be1db.chunk.js.map
